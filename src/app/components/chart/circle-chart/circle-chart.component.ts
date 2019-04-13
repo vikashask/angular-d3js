612ad48527294e7401a5d5306a9d5a0365b5647c
vikash <vikashraj144@gmail.com>
2019-04-14 00:22:43 +0530

    doc up

--- src/app/components/chart/circle-chart/circle-chart.component.ts
+++ src/app/components/chart/circle-chart/circle-chart.component.ts
@@ -11,54 +11,54 @@ export class CircleChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-//  Define variables 
+    //  Define variables 
     var width = 400;
-   var height = 400;
-   var data = [10, 20, 30];
-   var colors = ['green', 'purple', 'yellow'];
+    var height = 400;
+    var data = [10, 20, 30];
+    var colors = ['green', 'purple', 'yellow'];
 
-  //  Append SVG elements
-  var svg = d3.select("body")
-   .append("svg")
-   .attr("width", width)
-   .attr("height", height);
+    //  Append SVG elements
+    var svg = d3.select("body")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-  //  Apply transformation
-  var g = svg.selectAll('g')  //Creates group element to hold the circles.
-  .data(data)                 // Binds our data array to the group elements.
-  .enter()                    // Creates placeholders for our group elements.
-  .append('g')                // Appends group elements to our page.
-  .attr('transform',function(d,i){
-    return "translate(0,0)";
-  })        // translate is used to position your elements with respect to the origin.
-  
-  //Append circle elements
-  g.append('circle')
-  .attr('cx',function (d,i) {
-    return i*75+50;
-  })      //add the attributes to the group 
-  .attr('cy',function(d,i){
-    return 75;
-  })
-  .attr('r',function(d,i){
-    return d*1.5;
-  })
-  .attr('fill',function(d,i){
-    return colors[i];
-  })
+    //  Apply transformation
+    var g = svg.selectAll('g')  //Creates group element to hold the circles.
+      .data(data)                 // Binds our data array to the group elements.
+      .enter()                    // Creates placeholders for our group elements.
+      .append('g')                // Appends group elements to our page.
+      .attr('transform', function (d, i) {
+        return "translate(0,0)";
+      })        // translate is used to position your elements with respect to the origin.
 
-// Display data
-g.append('text')
-.attr('x',function(d,i){
-  return i*75+25;
-})
-.attr('y',80)
-.attr('stroke','teal')
-.attr('font-size','10px')
-.attr('font-family','sans-serif')
-.text(function(d){
-  return d;
-})
+    //Append circle elements
+    g.append('circle')
+      .attr('cx', function (d, i) {
+        return i * 75 + 50;
+      })      //add the attributes to the group 
+      .attr('cy', function (d, i) {
+        return 75;
+      })
+      .attr('r', function (d, i) {
+        return d * 1.5;
+      })
+      .attr('fill', function (d, i) {
+        return colors[i];
+      })
+
+    // Display data
+    g.append('text')
+      .attr('x', function (d, i) {
+        return i * 75 + 25;
+      })
+      .attr('y', 80)
+      .attr('stroke', 'teal')
+      .attr('font-size', '10px')
+      .attr('font-family', 'sans-serif')
+      .text(function (d) {
+        return d;
+      })
 
 
   }
