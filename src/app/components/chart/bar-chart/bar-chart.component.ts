a7f72e2533fa70a2cca9e7223ae3bb53d17d513c
vikash <vikashraj144@gmail.com>
2019-04-14 12:38:51 +0530

    bar chart code change

--- src/app/components/chart/bar-chart/bar-chart.component.ts
+++ src/app/components/chart/bar-chart/bar-chart.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import * as d3 from 'd3';
+
+import * as d3 from 'd3-selection';
+import * as d3Scale from 'd3-scale';
+import * as d3Array from 'd3-array';
+import * as d3Axis from 'd3-axis';
+
+import { STATISTICS } from '../../../shared';
 
 @Component({
   selector: 'app-bar-chart',
@@ -9,45 +15,101 @@ import * as d3 from 'd3';
 })
 export class BarChartComponent implements OnInit {
 
+  title = 'Bar Chart';
+
+  private width: number;
+  private height: number;
+  private margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+  private x: any;
+  private y: any;
+  private svg: any;
+  private g: any;
+
   constructor() { }
 
   ngOnInit() {
 
     // declaring variable
-    const data = [10, 5, 12, 15];
-    const width = 300,
-      scaleFactor = 20,
-      barHeight = 30;
+    // const data = [10, 5, 12, 15];
+    // const width = 300,
+    //   scaleFactor = 20,
+    //   barHeight = 30;
 
-    // appending svg element
-    const graph = d3.select('p')
-      .append('svg')
-      .attr('width', width)
-      .attr('height', barHeight * data.length);
+    // // appending svg element
+    // const graph = d3.select('p')
+    //   .append('svg')
+    //   .attr('width', width)
+    //   .attr('height', barHeight * data.length);
 
-    // Apply transformation
-    const bar = graph.selectAll('g')
-      .data(data)
-      .enter()
-      .append('g')
-      .attr('transform', function (d, i) {
-        return 'translate(0,' + i * barHeight + ')';
-      });
+    // // Apply transformation
+    // const bar = graph.selectAll('g')
+    //   .data(data)
+    //   .enter()
+    //   .append('g')
+    //   .attr('transform', function (d, i) {
+    //     return 'translate(0,' + i * barHeight + ')';
+    //   });
 
-    // append reactangle element to the bar
-    bar.append('rect')
-      .attr('width', function (d) {
-        return d * scaleFactor;
-      })
-      .attr('height', barHeight - 1);
+    // // append reactangle element to the bar
+    // bar.append('rect')
+    //   .attr('width', function (d) {
+    //     return d * scaleFactor;
+    //   })
+    //   .attr('height', barHeight - 1);
 
-    // displaying data
-    bar.append('text')
-      .attr('x', function (d) { return (d * scaleFactor); })
-      .attr('y', barHeight / 2)
-      .attr('dy', '.35em')
-      .text(function (d) { return d; });
+    // // displaying data
+    // bar.append('text')
+    //   .attr('x', function (d) { return (d * scaleFactor); })
+    //   .attr('y', barHeight / 2)
+    //   .attr('dy', '.35em')
+    //   .text(function (d) { return d; });
 
+    this.initSvg();
+    this.initAxis();
+    this.drawAxis();
+    this.drawBars();
   }
 
+  private initSvg() {
+    this.svg = d3.select('svg');
+    this.width = +this.svg.attr('width') - this.margin.left - this.margin.right;
+    this.height = +this.svg.attr('height') - this.margin.top - this.margin.bottom;
+    this.g = this.svg.append('g')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+  }
+
+  private initAxis() {
+    this.x = d3Scale.scaleBand().rangeRound([0, this.width]).padding(0.1);
+    this.y = d3Scale.scaleLinear().rangeRound([this.height, 0]);
+    this.x.domain(STATISTICS.map((d) => d.letter));
+    this.y.domain([0, d3Array.max(STATISTICS, (d) => d.frequency)]);
+  }
+  private drawAxis() {
+    this.g.append('g')
+        .attr('class', 'axis axis--x')
+        .attr('transform', 'translate(0,' + this.height + ')')
+        .call(d3Axis.axisBottom(this.x));
+    this.g.append('g')
+        .attr('class', 'axis axis--y')
+        .call(d3Axis.axisLeft(this.y).ticks(10, '%'))
+        .append('text')
+        .attr('class', 'axis-title')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 6)
+        .attr('dy', '0.71em')
+        .attr('text-anchor', 'end')
+        .text('Frequency');
+}
+
+private drawBars() {
+  this.g.selectAll('.bar')
+      .data(STATISTICS)
+      .enter().append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d) => this.x(d.letter) )
+      .attr('y', (d) => this.y(d.frequency) )
+      .attr('width', this.x.bandwidth())
+      .attr('height', (d) => this.height - this.y(d.frequency) );
+}
 }
