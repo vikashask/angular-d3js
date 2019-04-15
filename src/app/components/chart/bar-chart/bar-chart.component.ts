c9070261287a35cc2ada0edb68731ba0c12e801b
vikash <vikashraj144@gmail.com>
2019-04-15 18:27:16 +0530

    bar com

--- src/app/components/chart/bar-chart/bar-chart.component.ts
+++ src/app/components/chart/bar-chart/bar-chart.component.ts
@@ -30,41 +30,6 @@ export class BarChartComponent implements OnInit {
 
   ngOnInit() {
 
-    // declaring variable
-    // const data = [10, 5, 12, 15];
-    // const width = 300,
-    //   scaleFactor = 20,
-    //   barHeight = 30;
-
-    // // appending svg element
-    // const graph = d3.select('p')
-    //   .append('svg')
-    //   .attr('width', width)
-    //   .attr('height', barHeight * data.length);
-
-    // // Apply transformation
-    // const bar = graph.selectAll('g')
-    //   .data(data)
-    //   .enter()
-    //   .append('g')
-    //   .attr('transform', function (d, i) {
-    //     return 'translate(0,' + i * barHeight + ')';
-    //   });
-
-    // // append reactangle element to the bar
-    // bar.append('rect')
-    //   .attr('width', function (d) {
-    //     return d * scaleFactor;
-    //   })
-    //   .attr('height', barHeight - 1);
-
-    // // displaying data
-    // bar.append('text')
-    //   .attr('x', function (d) { return (d * scaleFactor); })
-    //   .attr('y', barHeight / 2)
-    //   .attr('dy', '.35em')
-    //   .text(function (d) { return d; });
-
     this.initSvg();
     this.initAxis();
     this.drawAxis();
@@ -72,9 +37,9 @@ export class BarChartComponent implements OnInit {
   }
 
   private initSvg() {
-    this.svg = d3.select('svg');
-    this.width = +this.svg.attr('width') - this.margin.left - this.margin.right;
-    this.height = +this.svg.attr('height') - this.margin.top - this.margin.bottom;
+    this.svg = d3.select('#chartarea');
+    this.width = +this.svg.attr('width') - this.margin.left - this.margin.right-300;
+    this.height = +this.svg.attr('height') - this.margin.top - this.margin.bottom-50;
     this.g = this.svg.append('g')
       .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
   }
@@ -107,9 +72,16 @@ private drawBars() {
       .data(STATISTICS)
       .enter().append('rect')
       .attr('class', 'bar')
+
+      .transition()
+        .duration(1500)
+        .delay(function(d,i){ return i*250}) // animation delay
+      
       .attr('x', (d) => this.x(d.letter) )
       .attr('y', (d) => this.y(d.frequency) )
       .attr('width', this.x.bandwidth())
+      
       .attr('height', (d) => this.height - this.y(d.frequency) );
+
 }
 }
