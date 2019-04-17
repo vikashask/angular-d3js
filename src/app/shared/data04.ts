593b0c1ac11f1bfe3adc139f7b2c51d3d9cd7857
vikash <vikashraj144@gmail.com>
2019-04-17 12:19:04 +0530

    data change

--- src/app/shared/data04.ts
+++ src/app/shared/data04.ts
@@ -1,7 +1,5 @@
 export const SAMPLE_DATA: any[] = [
-    {State: 'AL', 'Under 5 Years': 3, '5 to 13 Years': 5, '14 to 17 Years': 2, '18 to 24 Years': 4, '25 to 44 Years': 12, '45 to 64 Years': 12, '65 Years and Over': 6},
-    {State: 'AK', 'Under 5 Years': 5, '5 to 13 Years': 8, '14 to 17 Years': 4, '18 to 24 Years': 7, '25 to 44 Years': 19, '45 to 64 Years': 18, '65 Years and Over': 5},
-    {State: 'AZ', 'Under 5 Years': 5, '5 to 13 Years': 8, '14 to 17 Years': 3, '18 to 24 Years': 6, '25 to 44 Years': 18, '45 to 64 Years': 1, '65 Years and Over': 8},
-    {State: 'AR', 'Under 5 Years': 2, '5 to 13 Years': 3, '14 to 17 Years': 1, '18 to 24 Years': 2, '25 to 44 Years': 75, '45 to 64 Years': 7, '65 Years and Over': 40},
-    {State: 'CA', 'Under 5 Years': 2, '5 to 13 Years': 4, '14 to 17 Years': 2, '18 to 24 Years': 3, '25 to 44 Years': 10, '45 to 64 Years': 8, '65 Years and Over': 41},
+    {State: 'Directors and officers Liability','AIG': 12, 'HCC': 12, 'Zurich': 6},
+    {State: 'Employment Practices Liability', 'HCC': 18, 'Zurich': 5},
+    {State: 'Fiduciary Liabilities', 'HCC': 18, 'AIG': 8},
 ];
